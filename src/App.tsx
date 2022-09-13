17a96a8912081bfa0b35e7afe826ea1a74ce3828
Yuri Witney <yuriwfbarros@gmail.com>
2022-09-13 10:03:39 -0300

    feat: add background and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-interface ButtonProps {
-  title: string
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import './styles/main.css'
+import logoImg from './assets/logo-nlw-esports.svg'
 
 function App() {
   return (
-    <div>
-      <Button title= 'send 1'/>
-      <Button title= 'send 2'/>
-      <Button title= 'send 3'/>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logoImg} alt="" />
+
+      <h1 className='text-6xl text-white font-black mt-20'> 
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui. 
+      </h1>
     </div>
   )
 }
